32d562444a2bc58e92428c086f9a51519cbb7107
Gaurav Raj <techw803@gmail.com>
2021-05-14 23:07:03 +0530

    Updated the fonts of the Blog

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 export PATH=$PATH:/home/elliot/.local/share/gem/ruby/3.0.0/bin
+echo 'PATH Exported Successfully...'
 
 if [[ $# == 1 ]]; then
 	JEKYLL_ENV=production bundle exec jekyll b -d blog
 	$JEKYLL_ENV
-	# JEKYLL_ENV=production bundle exec jekyll b -d blog
+	echo 'Production Build Completed...'
 	cd blog
 	git add .
 	git commit -m "$1"
+	echo 'Committing the Changes to GitHub Completed...'
 	git push -u origin main
+	echo 'Blog Updated Successfully...'
 else
 	echo "Usage: $0 'Git Commit Message'"
 fi
